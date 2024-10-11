bbac2e888f0373c307d95581f8258f6076c4f62e
oscar sanchez <00oscarsanchez00@gmail.com>
2024-10-11 13:58:29 -0500

    added some moderation filtering for incoming comments

--- src/comments/CommentsList.jsx
+++ src/comments/CommentsList.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const CommentsList = ({ comments }) => {
   const renderComments = comments[0] ? (
     comments.map((comment) => {
+      let message = "";
+      if(comment.status === "pending") message = "pending ";
+      else if(comment.status === "rejected") message = "comment rejected";
+      else message = comment.content;
       return <li className='comment' key={comment.id}>
         <p className='comment-id'>{comment.id}:</p>
-        <p>{comment.content}</p>
+        <p>{message}</p>
         </li>;
     })
   ) : (
@@ -13,37 +17,4 @@ const CommentsList = ({ comments }) => {
   );
   return <ul className='comments-list'>{renderComments}</ul>;
 };
-
 export default CommentsList;
-
-
-
-/*
-const CommentsList = ({ comments }) => {
-  const [comments, setComments] = useState([]);
-
-  // the use of .then makes it unecessary to create a helper function
-  // and it stops lynter from complaining about dependencies in useEffect
-  useEffect(() => {
-      axios.get(`http://localhost:4001/posts/${postId}/comments`)
-      .then((e) => {
-        setComments(e.data);
-      })
-      .catch((e) => console.error(e));
-  }, [postId]);
-
-  const renderComments = comments[0] ? (
-    comments.map((comment) => {
-      return <li className='comment' key={comment.id}>
-        <p className='comment-id'>{comment.id}:</p>
-        <p>{comment.content}</p>
-        </li>;
-    })
-  ) : (
-    <li>... comments list is empty ...</li>
-  );
-
-  return <ul className='comments-list'>{renderComments}</ul>;
-};
-
-*/
